c3a03e82d42519e09cb6483d17ba6eb616032ea7
Liviu Preda <liviupreda@gmail.com>
2019-10-18 13:00:16 +0300

    add docs build

---
+++ docs/precache-manifest.0779b1a1963a738d55580ae5af6f21d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfcf0e65edc0f125974d40369f954873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b34fa111449ca874bc41",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c6629ef83cc175cb0ea6",
+    "url": "/static/css/main.b132cadc.chunk.css"
+  },
+  {
+    "revision": "b34fa111449ca874bc41",
+    "url": "/static/js/2.374bdcb7.chunk.js"
+  },
+  {
+    "revision": "c6629ef83cc175cb0ea6",
+    "url": "/static/js/main.02d36553.chunk.js"
+  },
+  {
+    "revision": "11ffe71f737934e50bc2",
+    "url": "/static/js/runtime-main.8d883a55.js"
+  },
+  {
+    "revision": "87466cd4f606eee3a29e9b9a78ef517a",
+    "url": "/static/media/brain.87466cd4.png"
+  }
+]);
