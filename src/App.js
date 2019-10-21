71420f07a450e6854e21745c34d3e02ad097e36f
Liviu Preda <liviupreda@gmail.com>
2019-10-21 10:41:08 +0300

    show user name and rank

--- src/App.js
+++ src/App.js
@@ -97,7 +97,22 @@ class App extends Component {
     this.setState({ imageUrl: this.state.input });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response => this.showFaceBox(this.calcFaceCoords(response)))
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(res => res.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            });
+        }
+        this.showFaceBox(this.calcFaceCoords(response));
+      })
       .catch(err => console.log(err));
   };
 
