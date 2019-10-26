7c1d0549449362697e176cdde0d189186b97f72e
Liviu Preda <liviupreda@gmail.com>
2019-10-26 15:00:10 +0300

    change build command, move to docs folder

---
+++ docs/precache-manifest.5350131369105d7ee2818a8c0fd09e39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660f9bc7cd1d6f502429bf3940b34106",
+    "url": "/js-react-node-smartbrain/index.html"
+  },
+  {
+    "revision": "1554cde3c1f3a41b15b9",
+    "url": "/js-react-node-smartbrain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "80b87be201a03cefdb99",
+    "url": "/js-react-node-smartbrain/static/css/main.b132cadc.chunk.css"
+  },
+  {
+    "revision": "1554cde3c1f3a41b15b9",
+    "url": "/js-react-node-smartbrain/static/js/2.2ef18312.chunk.js"
+  },
+  {
+    "revision": "80b87be201a03cefdb99",
+    "url": "/js-react-node-smartbrain/static/js/main.6cd1c556.chunk.js"
+  },
+  {
+    "revision": "d1f724f3484d30a69d3a",
+    "url": "/js-react-node-smartbrain/static/js/runtime-main.0b2d3d12.js"
+  },
+  {
+    "revision": "87466cd4f606eee3a29e9b9a78ef517a",
+    "url": "/js-react-node-smartbrain/static/media/brain.87466cd4.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed3b199a614c4164b16962aea3f19215.js"
+  "/js-react-node-smartbrain/precache-manifest.5350131369105d7ee2818a8c0fd09e39.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/js-react-node-smartbrain/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
